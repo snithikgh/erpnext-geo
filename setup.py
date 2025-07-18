63f334812d97a10899a6b1177ff5ad7374be4dad
Sreeja <sreeja.gogula@snithik.com>
2025-07-18 10:51:32 +0530

    added files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='erpnext-geo',
+    description='erpnext-geo',
+    author='Sandeep',
+    author_email='sandeep@snithik.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=('frappe',),
+)
